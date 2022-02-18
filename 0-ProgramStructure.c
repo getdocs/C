59f80937e86880f23e18a450c5e022b845803724
getDocs() <99954228+getdocs@users.noreply.github.com>
2022-02-18 14:23:05 +0500

    Add files via upload

---
+++ 0-ProgramStructure.c
@@ -0,0 +1,18 @@
+/*
+* including standard input output library (stdio.h)
+* for using output function we need to include this
+* this library includes the definition of output function
+* this library tells the compiler how printf() works
+* what are the input parameters and what is the output
+*/
+#include<stdio.h>
+/*
+* main function is entry point of program
+* return type of function is void
+* which means this is not returning any value
+* input parameters are also not being passed
+* this function is neither taking any input nor returning output
+*/
+void main(){
+
+}
---
+++ 1-HelloWorld.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+/*
+* display 'Hello World' on console window
+*/
+  printf("Hello World");
+}
